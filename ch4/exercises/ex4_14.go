ebc7b24c1f15b0dcb1de87b3ebe5004716a0623e
Nicola 'tekNico' Larosa <nico@teknico.net>
2017-03-13 09:13:38 +0100

    Skipped exercise 4.14 too

---
+++ ch4/exercises/ex4_14.go
@@ -0,0 +1,12 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// Changes by Nicola 'tekNico' Larosa <gopl.io@teknico.net>.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Exercise 4.14, p. 117: Create a web server that queries GitHub once and
+// then allows navigation of the list of bug reports, milestones, and users.
+
+// Skipped for now.
+
+package main
+
+func main() {}
